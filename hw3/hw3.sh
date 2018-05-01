a8ab8397dc0e9601fb779a08bb3881024d5601aa
R06942098 <32538529+R06942098@users.noreply.github.com>
2018-05-01 20:12:24 +0800

    modify

--- hw3/hw3.sh
+++ hw3/hw3.sh
@@ -1,7 +1,12 @@
 
-@author: cengbowei
-#!/bin/bash 
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 18 13:24:21 2018
 
+@author: cengbowei
+"""
+#!/bin/bash 
 wget 'https://www.dropbox.com/s/tcszv1q0z7bldm9/fc32.ckpt.data-00000-of-00001?dl=1' -O'fc32_vgg.ckpt.data-00000-of-00001'
 wget 'https://www.dropbox.com/s/u9vol2bflwsx3gn/fc32.ckpt.index?dl=1' -O'fc32_vgg.ckpt.index'
 wget 'https://www.dropbox.com/s/63efnpwpdltsqy7/fc32.ckpt.meta?dl=1' -O'fc32_vgg.ckpt.meta'
--- hw3/hw3_best.sh
+++ hw3/hw3_best.sh
@@ -1,4 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 18 13:24:21 2018
+
 @author: cengbowei
+"""
 #!/bin/bash 
 wget 'https://www.dropbox.com/s/unbetnya2kfvlzf/fc16_vgg.ckpt.data-00000-of-00001?dl=1' -O'fc16_vgg.ckpt.data-00000-of-00001'
 wget 'https://www.dropbox.com/s/une9q1ftlmw6eeq/fc16_vgg.ckpt.index?dl=1' -O'fc16_vgg.ckpt.index'
