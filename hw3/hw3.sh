9774a1bc5be3d5f87892184f5ae6667c9b8cd7ed
R06942098 <32538529+R06942098@users.noreply.github.com>
2018-05-02 21:14:27 +0800

    mo

--- hw3/hw3.sh
+++ hw3/hw3.sh
@@ -10,4 +10,4 @@ Created on Thu Jan 18 13:24:21 2018
 wget 'https://www.dropbox.com/s/tcszv1q0z7bldm9/fc32.ckpt.data-00000-of-00001?dl=1' -O'fc32_vgg.ckpt.data-00000-of-00001'
 wget 'https://www.dropbox.com/s/u9vol2bflwsx3gn/fc32.ckpt.index?dl=1' -O'fc32_vgg.ckpt.index'
 wget 'https://www.dropbox.com/s/63efnpwpdltsqy7/fc32.ckpt.meta?dl=1' -O'fc32_vgg.ckpt.meta'
-python hw3.py $1 $2 
+python3 hw3.py $1 $2 
